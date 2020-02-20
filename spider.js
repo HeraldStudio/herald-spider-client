1ca8d7ede01dbf6d158db19a7d952e628e74376c
ZZJ <zhaozjseu@163.com>
2020-02-20 23:48:00 +0800

    去除了手动模式，修改了爬虫认证的方式

--- spider.js
+++ spider.js
@@ -2,36 +2,46 @@
  * Created by WolfTungsten on 2018/2/5.
  */
 const ws = require('ws')
+const moment = require('moment')
 const axios = require('axios')
 const readline = require('readline');
 const tough = require('tough-cookie');
 const axiosCookieJarSupport = require('axios-cookiejar-support').default
+
+// config.json 中仅有小部分的配置有效
 const config = require('./config.json')
 const chalk = require('chalk')
-const input = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const crypto = require('crypto')
+
 axiosCookieJarSupport(axios)
 const program = require('commander');
 
 program
   .version('1.0.0')
-  .option('-m --manual', '手动连接')
   .option('-s --server <server>', '服务器地址')
   .option('-p, --port <port>', '端口号',parseInt)
   .option('-k --keyFile <keyFile>', '密钥文件')
   .parse(process.argv);
 
-const secret = require(program.keyFile);
+const spiderCommonConfig = (() => {
+    try {
+      return require(program.keyFile)
+    } catch (e) {
+      return ''
+    }
+})()
 /**
  ## 安全性
 
  由于学校部分 HTTPS 的上游服务器可能存在证书问题，这里需要关闭 SSL 安全验证。
  */
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-let isDefault = process.argv[2] === '--default'
+// let isDefault = process.argv[2] === '--default'
+
+// 256 color support
 const chalkColored = new chalk.constructor({level: 2});
+
+// 自定义一个console.log
 const log = (msg) => {
     if (config.serverLog) {
         console.log(JSON.stringify(Buffer.from(JSON.stringify({msg}))))
@@ -40,28 +50,26 @@ const log = (msg) => {
     }
 };
 
+// 爬虫 token 解密
+const decryptSipder = (value) => {
+    try {
+      let decipheriv = crypto.createDecipheriv(spiderCommonConfig.cipher, spiderCommonConfig.key, spiderCommonConfig.iv)
+      let result = decipheriv.update(value, 'hex', 'utf8')
+      result += decipheriv.final('utf8')
+      return result
+    } catch (e) {
+      console.log(e)
+      return ''
+    }
+  }
+
 class Spider {
     constructor() {
         log('>>>>> Herald-Spider 分布式硬件爬虫客户端 <<<<<')
         this.active = false;
         this.connected = false;
-        this.finalHeartBeat = (new Date).getTime();
-        if (program.manual) {
-            input.question(`${chalkColored.blue('[Input]')} 服务器地址 (${config.defaultServer}) `, (address) => {
-                if (address === '') {
-                    address = config.defaultServer
-                }
-                input.question(`${chalkColored.blue('[Input]')} 服务器地址 (${config.defaultPort}) `, (port) => {
-                    if (port === '') {
-                        port = config.defaultPort
-                    }
-                    port = parseInt(port)
-                    this.connect(address, port)
-                })
-            })
-        } else {
-            this.connect(program.server, program.port)
-        }
+        this.finalHeartBeat = +moment()
+        this.connect(program.server, program.port)
         this.statusQueue = [];
         this.succCounter = 0
     }
@@ -105,14 +113,14 @@ class Spider {
             process.exit(2)
         })
         this.socket.on('close', () => {
-            log('[-]服务器关闭');
+            log('[-] 服务器关闭');
             this.active = false;
             this.connected = false;
             process.exit(2)
         })
         this.socket.heartBeat = setInterval(() => {
             // 检服务器心跳是否正常
-            let currentTime = (new Date).getTime();
+            let currentTime = +moment();
             // 执行心跳逻辑
             if (currentTime - this.finalHeartBeat >= 10 * config.heartCycle) {
                 log(`爬虫${this.spiderName}由于服务器心跳超时退出`)
@@ -128,11 +136,10 @@ class Spider {
     handleData(data) {
         if (data === '@herald-server') {
             // 来自服务器的心跳拦截
-            this.finalHeartBeat = (new Date).getTime();
+            this.finalHeartBeat = +moment();
             //log(chalkColored.gray(`❤️服务器心跳: ${(new Date)}`));
             return;
         }
-
         if (this.active) {
             let request;
             try {
@@ -188,15 +195,6 @@ class Spider {
                         requestName: request.requestName,
                         succ: false,
                     };
-                    // try {
-                    //     preRes.status = error.response.status;
-                    //     preRes.statusText = error.response.statusText;
-                    //     preRes.headers = error.response.headers;
-                    //     preRes.message = error.message;
-                    // } catch (e) {}
-                    // if (error.response.hasOwnProperty('data')) {
-                    //     preRes.data = Buffer.from(error.response.data)
-                    // }
                     this.socket.send(JSON.stringify(preRes))
                     log(`${chalkColored.bold('<--')} ${chalkColored.cyan(request.requestName)} ${chalkColored.red('xxx')} ${chalkColored.red(request.url)} ${chalkColored.magenta(`succ:${this.failStatus()}`)} `)
                 } catch (e) {
@@ -211,18 +209,15 @@ class Spider {
             } else if (data === 'Auth_Fail') {
                 log(`${chalkColored.red('[-]')} 认证失败`)
                 process.exit(1)
-
             } else {
                 try {
                     this.spiderName = JSON.parse(data).spiderName
+                    let token = decryptSipder(JSON.parse(data).secret)
                     log(`${chalkColored.green('[+]')} 连接建立成功，spiderName=${this.spiderName} `);
-                    if (!program.manual) {
-                        this.socket.send(JSON.stringify({token: secret.token}))
-                    } else {
-                        input.question(`${chalkColored.blue('[Input]')} 登陆口令：`, (answer) => {
-                            this.socket.send(JSON.stringify({token: answer}))
-                        })
-                    }
+                    this.socket.send(JSON.stringify({
+                        spiderName: this.spiderName,
+                        token
+                    }))
                 } catch (e) {
                     console.error(e)
                 }
@@ -233,6 +228,6 @@ class Spider {
 
 setTimeout(() => {
     new Spider()
-}, 5 * 1000);
+}, 2 * 1000);
 
 
